d8d2fdab1ef725a4f19da92fe7b775eadfc5aa52
freive002 <freive00933@stud.noroff.no>
2024-11-16 19:06:56 +0100

    V1.4 Night message added

--- #Morning message.py
+++ #Morning message.py
@@ -1,11 +1,14 @@
 #V1.0 Morning message
-print("God morning! Nice whether today")
+print("Good morning! Nice whether today")
 
 #V1.1 Forenoon message
-print("God late morning, still nice wheter, stomarket is green today")
+print("Good late morning, still nice wheter, stomarket is green today")
 
 #V1.2 After noon message
-print("God After noon, is a bit more cloudy now, the stock is more nautral, now not a good day for the tek sector")
+print("Good After noon, is a bit more cloudy now, the stock is more nautral, now not a good day for the tek sector")
 
 #V1.3 Evening message
-print("God evening, close to rain now, the stock market closed bearly on red, this was not a good day for the tek sector")
+print("Good evening, close to rain now, the stock market closed bearly on red, this was not a good day for the tek sector")
+
+#V1.4 Evening message
+print("Good evening, it't raining!, 3Q report from tesla tommorw")
