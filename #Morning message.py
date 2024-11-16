c257b8dbe850844003e7454a2d2c4da93c6b145a
freive002 <freive00933@stud.noroff.no>
2024-11-16 18:54:24 +0100

    V 1.3 Evening message added

--- #Morning message.py
+++ #Morning message.py
@@ -6,3 +6,6 @@ print("God late morning, still nice wheter, stomarket is green today")
 
 #V1.2 After noon message
 print("God After noon, is a bit more cloudy now, the stock is more nautral, now not a good day for the tek sector")
+
+#V1.3 Evening message
+print("God evening, close to rain now, the stock market closed bearly on red, this was not a good day for the tek sector")
