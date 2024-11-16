883bb621e6fd3a582fd306bc473d2d1c3b95c6a3
freive002 <freive00933@stud.noroff.no>
2024-11-16 18:41:21 +0100

    V 1.2 After noon message is added

--- #Morning message.py
+++ #Morning message.py
@@ -3,3 +3,6 @@ print("God morning! Nice whether today")
 
 #V1.1 Forenoon message
 print("God late morning, still nice wheter, stomarket is green today")
+
+#V1.2 After noon message
+print("God After noon, is a bit more cloudy now, the stock is more nautral, now not a good day for the tek sector")
